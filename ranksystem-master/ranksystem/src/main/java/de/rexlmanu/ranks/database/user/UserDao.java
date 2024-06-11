b2d77ce5b8a372a486c230307ee278d908da8c98
Snowmansir <108475475+Snowmansir@users.noreply.github.com>
2024-06-11 05:09:00 +0200

    Delete ranksystem-master/ranksystem directory

--- ranksystem-master/ranksystem/src/main/java/de/rexlmanu/ranks/database/user/UserDao.java
+++
@@ -1,8 +0,0 @@
-package de.rexlmanu.ranks.database.user;
-
-import com.j256.ormlite.dao.Dao;
-import java.util.UUID;
-
-public interface UserDao extends Dao<UserEntity, Integer> {
-  UserEntity findByPlayer(UUID playerId);
-}
